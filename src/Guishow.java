800e939e309d9dcd2e024e8bc4319f929bab6a7c
tony148565 <51195315+tony148565@users.noreply.github.com>
2020-12-03 02:46:00 +0800

    Gui version 2.3
    separate two chessboards
    add textarea

--- src/Guishow.java
+++ src/Guishow.java
@@ -11,63 +11,28 @@ public class Guishow{
 	private JButton[][] chessBoardSquares = new JButton[10][10];
 	private JPanel chessBoardy,chessBoarde,chessBoard;
 	private Chessboard chessBoardz;
+	JScrollPane jScrollPane=null;
+	private JTextArea area = new JTextArea();
 	private static final String COLS = "ABCDEFGHIJK";
 	int i,j;
 	Guishow(){
 		initializeGui();
 	}
-	/*public JPanel mkchessboard(){
-		chessBoard = new JPanel(new GridLayout(0, 11));
-		chessBoard.setBorder(new LineBorder(Color.BLACK));
-		gui.add(chessBoard);
-		Insets buttonMargin = new Insets(0,0,0,0);
-		for(i = 0; i < chessBoardSquares.length; i++){
-			for(j = 0; j < chessBoardSquares[i].length ; j++){
-				int x=i+1;
-				int y=j+1;
-				JButton b = new JButton();  // make new button 
-				b.setMargin(buttonMargin);
-				b.setBackground(Color.BLUE);
-				ActionListener buttonListener = new ActionListener() {
-                    public void actionPerformed(ActionEvent e) {
-                        //pressedButton(ty, tx);
-						System.out.println("get!! x is " + x + " y is " + y);
-                    }
-			};
-			chessBoardSquares[j][i] = b;
-			chessBoardSquares[j][i].addActionListener(buttonListener);
-		}
-		}
-		chessBoard.add(new JLabel(""));
-		for (i = 0; i < 10; i++) {
-            chessBoard.add(new JLabel(COLS.substring(i, i + 1),SwingConstants.CENTER));
-        }
-		//chessBoard.add(new JLabel(""));
-		//---------------------------------------------------
-		for (i = 0; i < 10; i++) {
-            for (j = 0; j < 10; j++) {
-                switch (j) {
-                    case 0:
-                        chessBoard.add(new JLabel("" + (i + 1),SwingConstants.CENTER));
-                    default:
-                        chessBoard.add(chessBoardSquares[j][i]);
-                }
-            }
-        }
-		return chessBoard;
-	}*/
 	public final void initializeGui(){
 		gui.setBorder(new EmptyBorder(5, 5, 5, 5)); // make border style
 		chessBoardy = new JPanel(new GridLayout(0, 11));
 		chessBoardy.setBorder(new LineBorder(Color.BLACK));
 		chessBoardz = new Chessboard('y');
 		chessBoardy=chessBoardz.getChessBoard();
-		gui.add(chessBoardy);
-		chessBoarde = new JPanel(new GridLayout(0, 11));
+		gui.add(chessBoardy,BorderLayout.WEST);
+		area.setLineWrap(true);
+		jScrollPane = new JScrollPane(area);
+		gui.add(jScrollPane,BorderLayout.CENTER);
+		chessBoarde = new JPanel(new GridLayout(1, 11));
 		chessBoarde.setBorder(new LineBorder(Color.BLACK));
 		chessBoardz = new Chessboard('e');
 		chessBoarde=chessBoardz.getChessBoard();
-		gui.add(chessBoarde);
+		gui.add(chessBoarde,BorderLayout.EAST);
 		
 		//chessBoarde = new mkchessboard();
 		//---------------------------------------------------
@@ -117,7 +82,7 @@ class Chessboard extends JPanel{// make chessboard
 	public void mkchessboard(){
 		chessBoard = new JPanel(new GridLayout(0, 11)); // let chessBoard has 11 cols
 		chessBoard.setBorder(new LineBorder(Color.BLACK));
-		Insets buttonMargin = new Insets(0,0,0,0); // define button sizes **I Can't explain how it work
+		Insets buttonMargin = new Insets(5,5,5,5); // define button sizes **I Can't explain how it work
 		for(i = 0; i < chessBoardSquares.length; i++){ 
 			for(j = 0; j < chessBoardSquares[i].length ; j++){
 				char t = a; // check 
