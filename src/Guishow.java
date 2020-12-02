aba19e55a16f84b63cadcf9100313183cae41f36
tony148565 <51195315+tony148565@users.noreply.github.com>
2020-12-02 22:52:05 +0800

    Gui version 2.0
    add simple chessboard

--- src/Guishow.java
+++ src/Guishow.java
@@ -1,17 +1,66 @@
 import java.awt.*;
+import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import javax.swing.*;
 import javax.swing.border.*;
 
 public class Guishow{
+	
 	private final JPanel gui = new JPanel(new BorderLayout(3, 3));
+	private JButton[][] chessBoardSquares = new JButton[10][10];
+	private JPanel chessBoard;
+	private static final String COLS = "ABCDEFGHIJK";
+	int i,j;
 	Guishow(){
 		initializeGui();
 	}
+	
 	public final void initializeGui(){
-		gui.setBorder(new EmptyBorder(5, 5, 5, 5));
+		gui.setBorder(new EmptyBorder(5, 5, 5, 5)); // make border style
+		chessBoard = new JPanel(new GridLayout(0, 11));
+		chessBoard.setBorder(new LineBorder(Color.BLACK));
+		gui.add(chessBoard);
 		
-	}
+		Insets buttonMargin = new Insets(0,0,0,0);
+		for(i = 0; i < chessBoardSquares.length; i++){
+			for(j = 0; j < chessBoardSquares[i].length ; j++){
+				int x=i+1;
+				int y=j+1;
+				JButton b = new JButton();  // make new button 
+				b.setMargin(buttonMargin);
+				b.setBackground(Color.BLUE);
+				ActionListener buttonListener = new ActionListener() {
+                    public void actionPerformed(ActionEvent e) {
+                        //pressedButton(ty, tx);
+						System.out.println("get!! x is " + x + " y is " + y);
+                    }
+			};
+			chessBoardSquares[j][i] = b;
+			chessBoardSquares[j][i].addActionListener(buttonListener);
+		}
+		}
+		chessBoard.add(new JLabel(""));
+		for (i = 0; i < 10; i++) {
+            chessBoard.add(new JLabel(COLS.substring(i, i + 1),SwingConstants.CENTER));
+        }
+		chessBoard.add(new JLabel(""));
+		//---------------------------------------------------
+		for (i = 0; i < 10; i++) {
+            for (j = 0; j < 10; j++) {
+                switch (j) {
+                    case 0:
+                        chessBoard.add(new JLabel("" + (i + 1),SwingConstants.CENTER));
+                    default:
+                        chessBoard.add(chessBoardSquares[j][i]);
+                }
+            }
+        }
+		//---------------------------------------------------
+		}
+	
+	public final JComponent getChessBoard() {
+        return chessBoard;
+    }
 	public final JComponent getGui() {
         return gui;
     }
