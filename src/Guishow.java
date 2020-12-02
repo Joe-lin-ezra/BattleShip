45cfd6bf0646b36cb6eb5aff21770d85a95ee1e8
tony148565 <51195315+tony148565@users.noreply.github.com>
2020-12-02 22:59:47 +0800

    Gui version 2.1
    fix chesBoard

--- src/Guishow.java
+++ src/Guishow.java
@@ -8,19 +8,16 @@ public class Guishow{
 	
 	private final JPanel gui = new JPanel(new BorderLayout(3, 3));
 	private JButton[][] chessBoardSquares = new JButton[10][10];
-	private JPanel chessBoard;
+	private JPanel chessBoardy,chessBoarde,chessBoard;
 	private static final String COLS = "ABCDEFGHIJK";
 	int i,j;
 	Guishow(){
 		initializeGui();
 	}
-	
-	public final void initializeGui(){
-		gui.setBorder(new EmptyBorder(5, 5, 5, 5)); // make border style
+	public JPanel mkchessboard(){
 		chessBoard = new JPanel(new GridLayout(0, 11));
 		chessBoard.setBorder(new LineBorder(Color.BLACK));
 		gui.add(chessBoard);
-		
 		Insets buttonMargin = new Insets(0,0,0,0);
 		for(i = 0; i < chessBoardSquares.length; i++){
 			for(j = 0; j < chessBoardSquares[i].length ; j++){
@@ -43,7 +40,7 @@ public class Guishow{
 		for (i = 0; i < 10; i++) {
             chessBoard.add(new JLabel(COLS.substring(i, i + 1),SwingConstants.CENTER));
         }
-		chessBoard.add(new JLabel(""));
+		//chessBoard.add(new JLabel(""));
 		//---------------------------------------------------
 		for (i = 0; i < 10; i++) {
             for (j = 0; j < 10; j++) {
@@ -55,6 +52,12 @@ public class Guishow{
                 }
             }
         }
+		return chessBoard;
+	}
+	public final void initializeGui(){
+		gui.setBorder(new EmptyBorder(5, 5, 5, 5)); // make border style
+		chessBoardy = mkchessboard();
+		//chessBoarde = new mkchessboard();
 		//---------------------------------------------------
 		}
 	
