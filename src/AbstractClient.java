36043d419842c2980db285c7bf39080a011cf06f
tony148565 <51195315+tony148565@users.noreply.github.com>
2021-01-13 00:39:55 +0800

    Add files via upload

---
+++ src/AbstractClient.java
@@ -0,0 +1,8 @@
+
+
+public abstract class AbstractClient{
+	public abstract boolean login(String name);
+	public abstract boolean join();
+	public abstract boolean getroomstate();
+	public abstract String isAlive();
+}
